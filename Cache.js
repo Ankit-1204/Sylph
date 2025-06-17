857ad52cc16a0ddc2837bee6e97d2c3069ab6b40
Ankit-1204 <ankit2003upadhyay@gmail.com>
2025-06-18 00:23:46 +0530

    Normalizing caching path

--- Cache.js
+++ Cache.js
@@ -10,4 +10,12 @@ class Cache{
         let url=new URL(req.url);
         
     }
+    parseUrl(url){
+        const u=new URL(url);
+        const params=[...u.searchParams.entries()]   // this gives something like -> [['a',1], ['b',2]]
+        const sortedParams=params.sort((a,b)=>a[0].localeCompare(b[0])) // basically a comparater
+        const paramsString=new URLSearchParams(sortedParams).toString()
+        const normalisedPath=u.pathname + (paramsString ? `${paramsString}`:"");
+        return normalisedPath;
+    }
 }
